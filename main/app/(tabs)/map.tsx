e3ff0c64541015feb7a77b88a200d71b7db527a9
Gibraltar-47 <malak.sefriou@gmail.com>
2026-01-28 20:48:18 -0500

    function map fixed for naming consistency

--- main/app/(tabs)/map.tsx
+++ main/app/(tabs)/map.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import MapView from "react-native-maps";
 
-export default function Map() {
+export default function MapScreen() {
   return (
     <View style={styles.container}>
       <MapView
