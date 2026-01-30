a70a7f62a582e44451aed2429ac2837c86f51e42
Gibraltar-47 <malak.sefriou@gmail.com>
2026-01-29 23:41:33 -0500

    added code for sgw buildings shapes and colors

--- main/app/(tabs)/map.tsx
+++ main/app/(tabs)/map.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, {Marker, Polygon} from "react-native-maps";
+import { Building, buildingCatalog } from "@/models/Building";
 
 export default function MapScreen() {
   return (
@@ -13,7 +14,18 @@ export default function MapScreen() {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+      
+      {buildingCatalog.buildings.map((building:Building) => (
+          <Polygon 
+          coordinates={building.polygon}
+          key={building.name}
+          strokeColor="#000"
+          fillColor="rgba(96, 6, 6, 0.52)"
+        />
+      ))}
+
+      </MapView>
     </View>
   );
 }
---
+++ main/models/Campus.ts
@@ -0,0 +1,51 @@
+import { LatLng } from "./Building";
+
+export class CampusCatalog {
+    campuses: Campus[];
+
+    constructor() {
+        this.campuses = [];
+    }
+
+    addCampus(campus: Campus) {
+        this.campuses.push(campus);
+    }
+
+    addCampusByData(name: string, center: LatLng, address: string, codename: string) {
+        const newCampus = new Campus(name, center, address, codename);
+        this.campuses.push(newCampus);
+    }
+}
+
+export class Campus {
+    name: string;
+    center: LatLng;
+    address: string;
+    codename: string;
+
+    constructor(name: string, center: LatLng, address: string, codename: string) {
+        this.name = name;
+        this.center = center;
+        this.address = address;
+        this.codename = codename;
+    }
+}
+
+export const campusCatalog = new CampusCatalog();
+
+export const sgwCampus = new Campus(
+    "Sir George Williams Campus",
+    {latitude: 45.49711571, longitude: -73.57881003},
+    "1455 De Maisonneuve Blvd W, Montreal, QC H3G 1M8, Canada",
+    "SGW"
+);
+
+export const loyolaCampus = new Campus(
+    "Loyola Campus",
+    {latitude: 45.4585720638379, longitude: -73.6391689330469},
+    "7141 Sherbrooke St W, Montreal, QC H4B 1R6, Canada",
+    "LOY"
+);
+
+campusCatalog.addCampus(sgwCampus);
+campusCatalog.addCampus(loyolaCampus);
