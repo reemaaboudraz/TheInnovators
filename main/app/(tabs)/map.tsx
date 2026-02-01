81d1e5d7916734d9e39aa1fb5401fec342d7ceb5
fouadM200 <124646166+fouadM200@users.noreply.github.com>
2026-02-01 11:51:14 -0500

    changing branch name only

--- main/app/(tabs)/map.tsx
+++ main/app/(tabs)/map.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import SGWCampus from "@/components/campus/SGW/SGWCampus";
 import LoyolaCampus from "@/components/campus/Loyola/LoyolaCampus";
 
-// this will be removed when the toggle button is added later on
+// this will be removed when the toggle button is added later on...
 const DEFAULT_CAMPUS: "SGW" | "LOY" = "SGW";
 
 export default function MapScreen() {
